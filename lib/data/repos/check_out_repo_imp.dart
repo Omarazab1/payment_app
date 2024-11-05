8ef8072b3264a25167ef3b0447239ac1fb0727c1
Omarazab1 <oazab6806@gmail.com>
2024-11-05 23:29:33 +0200

    create check_out_repo & check_out_repo_impl

---
+++ lib/data/repos/check_out_repo_imp.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+
+import 'package:paymentapp/core/utils/failures.dart';
+import 'package:paymentapp/core/utils/stripe_service.dart';
+
+import 'package:paymentapp/data/models/payment_intent_model/payment_intent_input_model.dart';
+
+import 'check_out_repo.dart';
+
+class CheckOutRepoImp  extends CheckOutRepo{
+  final StripeService stripeService;
+
+  CheckOutRepoImp({required this.stripeService});
+  @override
+  Future<Either<Failure, void>> makePayment({required PaymentIntentInputModel paymentIntentInputModel}) async{ {
+    try {
+      await stripeService.makePayment(paymentIntentInputModel: paymentIntentInputModel);
+      return const Right(null);
+    }catch(e){
+      return Left(ServerFailure(errMessage: e.toString()));
+    }
+  }
+}}
